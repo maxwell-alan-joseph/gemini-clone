0b077640da64cc4ffb93a305e43cc99786645666
Maxwell Alan Joseph <maxwellalanjoseph@gmail.com>
2025-09-24 20:15:58 +0530

    sidebar

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -11,9 +11,27 @@ const Sidebar = () => {
                 <img src={assets.plus_icon} alt="" />
                 <p>New Chat</p>
             </div>
+            <div className="recent">
+                <p className="recent-title">Recent</p>
+                <div className="recent-entry">
+                    <img src={assets.message_icon} alt="" />
+                    <p>What is react...</p>
+                </div>
+            </div>
         </div>
         <div className="bottom">
-
+            <div className="bottom-item recent-entry">
+                <img src={assets.question_icon} alt="" />
+                <p>Help</p>
+            </div>
+            <div className="bottom-item recent-entry">
+                <img src={assets.history_icon} alt="" />
+                <p>History</p>
+            </div>
+            <div className="bottom-item recent-entry">
+                <img src={assets.setting_icon} alt="" />
+                <p>Settings</p>
+            </div>
         </div>
     </div>
   )
