248025eb6be2272f69b1491ac651969cc4fee8f3
Maxwell Alan Joseph <maxwellalanjoseph@gmail.com>
2025-09-24 21:50:03 +0530

    setExtended-React

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
-import React from 'react'
-import Sidebar from './components/sidebar/Sidebar'
-
+import React from "react";
+import Sidebar from "./components/sidebar/Sidebar";
+import Main from "./components/main/main";
 
 const App = () => {
- return (
-  <>
-    <Sidebar/>
-  </>
- )
-}
+  return (
+    <>
+      <Sidebar />
+      <Main />
+    </>
+  );
+};
 
-export default App
+export default App;
